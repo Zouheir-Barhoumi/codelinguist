5381e1121a5e040c8906ad790c9f6e6a4d90b000
zck <zck@core>
2024-12-24 16:39:44 +0100

    slightly changed mission texté

--- pages/index.vue
+++ pages/index.vue
@@ -164,7 +164,7 @@ export default {
           description: "Short summary...",
         },
       ],
-      missionText: `At TheCodeLinguist, we're not just focused on code writing—we believe that programming mastery isn't all about abstract algorithms or overly complex projects either. Instead, we focus on the small, powerful pieces of functionality that are the building blocks of every application. By breaking down concepts to low-level functions we aim to provide practical, reusable skills that can be applied across countless programming scenarios. Our mission is to make coding accessible by transforming complex ideas into clear insights—enabling you to learn and build real programming skills, one line at a time.`,
+      missionText: `At TheCodeLinguist, our method isn't restricted to code writing—we believe that programming mastery isn't all about abstract algorithms or overly complex projects either. Instead, we focus on the small, powerful pieces of functionality that are the building blocks of every application. By breaking down concepts to low-level functions we aim to provide practical, reusable skills that can be applied across countless programming scenarios. Our mission is to make coding accessible by transforming complex ideas into clear insights—enabling you to learn and build real programming skills, one line at a time.`,
     };
   },
   methods: {
