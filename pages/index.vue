e848837b50e98054f8f5b6d755b7256a874f1587
zck <zck@core>
2025-01-07 01:49:07 +0100

    gave max width to blog section inner div + made blog card a standalone component + made paginationstandalone

--- pages/index.vue
+++ pages/index.vue
@@ -136,69 +136,20 @@
         >
           Blog
         </h2>
-        <div class="flex flex-wrap justify-center gap-6">
+        <div class="flex flex-wrap justify-center gap-6 max-w-5xl">
           <!-- Blog Cards -->
-          <div
-            v-for="post in pageItems"
-            :key="post.id"
-            class="mh-60 h-80 m:h-80 w-80 border border-solid border-2 bg-lighter backdrop-blur-md hover:shadow-2xl flex flex-col justify-between relative hover:scale-105 transition-transform duration-300 ease-in-out p-2"
-            :style="getBorderStyle(post.id)"
-          >
-            <!-- Background Image -->
-            <div
-              class="absolute top-0 left-0 h-full w-full bg-cover bg-center opacity-12"
-              :style="{ backgroundImage: `url(${post.image})` }"
-              alt="blog post image"
-            ></div>
-            <!-- Header -->
-            <div class="h-4 bg-white"></div>
-            <!-- Content -->
-            <div class="relative z-10 p-4 flex flex-col h-full justify-center">
-              <h3 class="text-lg font-bold mb-2">{{ post.title }}</h3>
-              <p class="text-sm text-grey mb-4 text-justify">
-                {{ post.excerpt }}
-              </p>
-              <div class="flex justify-end">
-                <nuxt-link
-                  :to="`/blog/${post.id}`"
-                  class="text-darkest bg-tertiary px-4 py-2 rounded-sm cursor-pointer"
-                >
-                  Read more
-                </nuxt-link>
-              </div>
-            </div>
-            <!-- Footer -->
-            <div
-              class="relative z-10 flex justify-between items-center text-sm text-dark p-2 mt-4 bg-light"
-            >
-              <span>{{ post.topics.join(" | ") }}</span>
-              <span>{{ post.date }}</span>
-            </div>
+          <div v-for="post in pageItems" :key="post.id">
+            <BlogCard :post="post" />
           </div>
         </div>
       </div>
       <!-- Pagination Controls -->
-      <div
-        class="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-80 flex justify-between items-center"
-      >
-        <button
-          @click="prevPage"
-          :disabled="currentPage === 1"
-          class="px-4 py-2 text-darkest font-bold disabled:text-warning disabled:font-normal disabled:cursor-not-allowed"
-        >
-          Prev
-        </button>
-        <span class="text-grey">
-          Page {{ currentPage }} of {{ totalPages }}
-        </span>
-        <button
-          @click="nextPage"
-          :disabled="currentPage === totalPages"
-          class="px-4 py-2 text-darkest font-bold disabled:text-warning disabled:font-normal disabled:cursor-not-allowed"
-        >
-          Next
-        </button>
-      </div>
+      <PaginationSection
+        :currentPage="currentPage"
+        :totalPages="totalPages"
+        @prevPage="prevPage"
+        @nextPage="nextPage"
+      />
     </section>
 
     <!-- Projects Section -->
@@ -268,6 +219,7 @@
 </template>
 
 <script setup>
+import "~/components/BlogCard.vue";
 const route = useRoute();
 const isActive = (linkPath) => route.path == linkPath;
 let isMenuOpen = ref(false);
